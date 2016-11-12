11bd3f07e85d7d4f376be528f1380ab21de9fd90
masterUNG <saytanpotong@gmail.com>
2016-11-12 16:22:04 +0700

    เลือกรูป

--- app/src/main/java/kku/photong/saitan/androidung1/SignUpActivity.java
+++ app/src/main/java/kku/photong/saitan/androidung1/SignUpActivity.java
@@ -1,10 +1,12 @@
 package kku.photong.saitan.androidung1;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,8 +22,9 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText nameEditText,phonEditText,userEditText, passwordEditText;
     private ImageView imageView;
     private Button button;
-    private String nameString, phonString, userString, passString;
+    private String nameString, phonString, userString, passString,imagePathString,imageNameString;
     private Uri uri;
+    private boolean aBoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {// create sign Up
@@ -46,13 +49,24 @@ public class SignUpActivity extends AppCompatActivity {
                 passString = passwordEditText.getText().toString().trim();
 
                 //Check Space
-                if (nameString.equals("") || phonString.equals("") || userString.equals("")||passString.equals("")){
+                if (nameString.equals("") || phonString.equals("") || userString.equals("") || passString.equals("")) {
                     //have Space
-                    Log.d("12novV1","Have Space"); // debugging
+                    Log.d("12novV1", "Have Space"); // debugging
                     MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48, "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่อง");
                     myAlert.myDialog();
 
+                } else if(aBoolean){
+                    //Non Choose Image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.nobita48, "ยังไม่ได้เลือกรูป", "กรุณาเลือกรูปด้้วยค่ะ");
+                    myAlert.myDialog();
+                }else {
+                    //Choose Image OK
+                    upLoadImageToServer();
                 }
+            }
+
+            private void upLoadImageToServer() {
+
             }
         });
 
@@ -78,6 +92,7 @@ public class SignUpActivity extends AppCompatActivity {
         if (( requestCode == 0)&&(resultCode==RESULT_OK)) {
 
             Log.d("12novV1","Result OK");
+            aBoolean = false;
             uri = data.getData();
             try {
                 Bitmap bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
@@ -86,8 +101,33 @@ public class SignUpActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-        }//if
+            // Find Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1","imagePast=>" + imagePathString);
 
+            //Find Name of Image
+            imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
+            Log.d("12novV1","imagePast=>" + imageNameString);
+
+        }//if
+    }
+
+    private String myFindPath(Uri uri) {
+
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            result = cursor.getString(index);
+
+        }else {
+            result = uri.getPath();
+        }
+
+        return result;
     }
 
 }// Main Class
